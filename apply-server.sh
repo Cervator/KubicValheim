49e70335d728ac6d815952d64a3c0d51a2f9192d
Rasmus Praestholm <rpraestholm@adaptavist.com>
2021-09-18 10:48:27 -0500

    Add a second server entry and a namespace

--- apply-server.sh
+++ apply-server.sh
@@ -2,14 +2,15 @@
 if [ $# -eq 1 ]
 then
   echo "Going to work with the Valheim server $1";
-  kubectl apply -f $1/valheim-pvc.yaml
-  kubectl apply -f valheim-player-lists-cm.yaml
+  kubectl create ns valheim
+  kubectl apply -f $1/valheim-pvc.yaml -n valheim
+  kubectl apply -f valheim-player-lists-cm.yaml -n valheim
   echo "Hope you remembered to update the passwords in the secrets file only locally!"
-  kubectl apply -f valheim-secrets.yaml
-  kubectl apply -f $1/valheim-service.yaml
-  kubectl apply -f $1/valheim-deployment.yaml
+  kubectl apply -f valheim-secrets.yaml -n valheim
+  kubectl apply -f $1/valheim-service.yaml -n valheim
+  kubectl apply -f $1/valheim-deployment.yaml -n valheim
   # TODO: Consider using a stateful set just to get a cleaner pod name? Only ever 0 or 1 instances ...
 else
   echo "Didn't get exactly one arg, got $# ! $*"
-  echo "Valid server names are: valheim1"
+  echo "Valid server names are: valheim1, valheim2"
 fi
--- delete.sh
+++ delete.sh
@@ -2,12 +2,12 @@
 if [ $# -eq 1 ]
 then
   echo "Going to delete the Valheim server $1";
-  kubectl delete -f $1/valheim-deployment.yaml
-  kubectl delete -f valheim-player-lists-cm.yaml
-  kubectl delete -f valheim-secrets.yaml
-  kubectl delete -f $1/valheim-service.yaml
-  kubectl delete -f $1/valheim-pvc.yaml
+  kubectl delete -f $1/valheim-deployment.yaml  -n valheim
+  kubectl delete -f valheim-player-lists-cm.yaml -n valheim
+  kubectl delete -f valheim-secrets.yaml -n valheim
+  kubectl delete -f $1/valheim-service.yaml -n valheim
+  kubectl delete -f $1/valheim-pvc.yaml -n valheim
 else
   echo "Didn't get exactly one arg, got $# ! $*"
-  echo "Valid server names are: valheim1"
+  echo "Valid server names are: valheim1, valheim2"
 fi
